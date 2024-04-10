d08b890b63bbdd83f79dafce08196fdae00ebe24
KRON1K <97955568+WEDALARISEWMEM@users.noreply.github.com>
2024-04-11 02:19:30 +0300

    Update oto-login.py

--- oto-login.py
+++ oto-login.py
@@ -33,8 +33,8 @@ except:
     pass  # CODER BY KRON1K
 
 # Oturum bilgileri
-phone_number = "5437331923"  # CODER BY KRON1K
-password = "515783"  # CODER BY KRON1K
+phone_number = "5555555555"  # CODER BY KRON1K
+password = "555555"  # CODER BY KRON1K
 
 # Telefon numarası kutucuğunun yüklenmesini bekleyin
 phone_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[placeholder="(XXX) XXX XX XX"]')))
